3e7a00703bedf710e565ac2a73c3a87dc0c92d42
nachosoloaga <nachosoloaga@gmail.com>
2021-06-07 13:24:12 -0300

    Fix circular dependency on UserService file

--- src/state/reducers/index.js
+++ src/state/reducers/index.js
@@ -1,21 +1,10 @@
 import { combineReducers } from 'redux';
-import localForage from 'localforage';
-import { persistReducer } from 'redux-persist';
-import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
-
+import session from './sessionReducer';
 import statusReducer from './statusReducer';
-import sessionReducer from './sessionReducer';
 import targetReducer from './targetReducer';
 
-const sessionPersistConfig = {
-  key: 'session',
-  storage: localForage,
-  whitelist: ['authenticated', 'info', 'user'],
-  stateReconciler: autoMergeLevel2
-};
-
 const rootReducer = combineReducers({
-  session: persistReducer(sessionPersistConfig, sessionReducer),
+  session,
   statusReducer,
   targetReducer
 });
