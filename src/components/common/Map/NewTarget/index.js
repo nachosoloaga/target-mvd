b65029a5ab1153f65bb47bd94d076a1e6ab15126
nachosoloaga <nachosoloaga@gmail.com>
2021-06-14 11:57:35 -0300

    Add limit for 10 active targets at the same time

--- src/components/common/Map/NewTarget/index.js
+++ src/components/common/Map/NewTarget/index.js
@@ -1,17 +1,25 @@
 import { useHistory } from 'react-router-dom';
 import { useMapEvents } from 'react-leaflet';
 import { useNewTarget } from 'hooks';
+import { useSelector } from 'react-redux';
+import { useIntl } from 'react-intl';
 
 const NewTarget = () => {
   const history = useHistory();
+  const intl = useIntl();
   const { updateTargetCoords } = useNewTarget();
+  const targetsLength = useSelector(state => state.targetReducer.targets.length);
 
   useMapEvents({
     click(e) {
-      const coordinates = [e.latlng.lat, e.latlng.lng];
-      updateTargetCoords(coordinates);
-      if (history.location.pathname != '/targets/new') {
-        history.push('/targets/new');
+      if (targetsLength <= 10) {
+        const coordinates = [e.latlng.lat, e.latlng.lng];
+        updateTargetCoords(coordinates);
+        if (history.location.pathname != '/targets/new') {
+          history.push('/targets/new');
+        }
+      } else {
+        window.alert(intl.formatMessage({ id: 'target.new.restriction' }));
       }
     }
   });
