731bdd59e5b72ab6ac8a60f425f20adef598f129
nachosoloaga <nachosoloaga@gmail.com>
2021-06-22 16:27:17 -0300

    Fix on configureStore file for prod env

--- src/state/store/configureStore.prod.js
+++ src/state/store/configureStore.prod.js
@@ -1,19 +1,12 @@
-import { persistStore } from 'redux-persist';
 import { configureStore } from '@reduxjs/toolkit';
 
 import reducer from 'state/reducers';
 
-export default ((initialState, isServerSide = false) => {
+export default (initialState => {
   const store = configureStore({
     reducer,
     preloadedState: initialState
   });
 
-  if (isServerSide) {
-    return { store };
-  }
-
-  const persistor = persistStore(store);
-
-  return { store, persistor };
+  return store;
 })();
